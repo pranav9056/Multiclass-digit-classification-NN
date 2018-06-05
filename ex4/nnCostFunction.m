21d99e081f80e4d9969fb4ddfebe2d505715486b
Pranav <pranavjain3194@gmail.com>
2018-06-05 10:17:15 -0400

    added regularization to gradients

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -51,63 +51,14 @@ for i = 1:m
    
 end
 
-Theta1_grad = Theta1_grad ./ m;
-Theta2_grad = Theta2_grad ./ m;
+Theta1_grad = Theta1_grad ./ m + (lambda/m)* [zeros(size(Theta1,1),1) Theta1(:,2:end)];
+Theta2_grad = Theta2_grad ./ m + (lambda/m)* [zeros(size(Theta2,1),1) Theta2(:,2:end)];
 
 
 J = J/m;
 RegularizedJ = sum(sum(Theta1(:,2:end) .^ 2)) + sum(sum(Theta2(:,2:end) .^ 2));
 RegularizedJ = lambda*RegularizedJ/(2*m);
 J = J + RegularizedJ; 
-% ====================== YOUR CODE HERE ======================
-% Instructions: You should complete the code by working through the
-%               following parts.
-%
-%
-% Part 2: Implement the backpropagation algorithm to compute the gradients
-%         Theta1_grad and Theta2_grad. You should return the partial derivatives of
-%         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
-%         Theta2_grad, respectively. After implementing Part 2, you can check
-%         that your implementation is correct by running checkNNGradients
-%
-%         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
-%               binary vector of 1's and 0's to be used with the neural network
-%               cost function.
-%
-%         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
-%               first time.
-%
-% Part 3: Implement regularization with the cost function and gradients.
-%
-%         Hint: You can implement this around the code for
-%               backpropagation. That is, you can compute the gradients for
-%               the regularization separately and then add them to Theta1_grad
-%               and Theta2_grad from Part 2.
-%
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% -------------------------------------------------------------
-
-% =========================================================================
 
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
